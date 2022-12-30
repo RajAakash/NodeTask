bdfd31c83a504f89d4994e30dbbcf00e93bfb49b
RajAakash <https://github.com/rajAakash01>
2022-12-30 16:01:17 +0545

    Ejs page render

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("./src/db/mongoose");
 const Task = require("./src/models/task");
+const taskRouter = require("./src/router/task");
 
 const app = express();
+
+app.use(taskRouter);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
+  res.render("list", { newListItem });
   Task.find({})
     .then((tasks) => {
       res.send(tasks);
@@ -41,6 +44,6 @@ app.post("/delete", (req, res) => {
   });
 });
 
-app.listen(3001, () => {
-  console.log("listening on port 3001.");
+app.listen(3000, () => {
+  console.log("listening on port 3000.");
 });
