1da5a01e5854b2687153065484540cb8335221a6
RajAakash <https://github.com/rajAakash01>
2022-12-31 10:48:19 +0545

    Separate router

--- app.js
+++ app.js
@@ -12,66 +12,71 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  // Promise.reject()
-  Task.find({})
-    .then((newListItem) => {
-      res.render("list", { newListItem });
-    })
-    .catch((e) => {
-      res.render("list", { error: true });
-    });
-});
+// app.get("/", (req, res) => {
+//   // Promise.reject()
+//   Task.find({})
+//     .then((newListItem) => {
+//       res.render("list", { newListItem });
+//     })
+//     .catch((e) => {
+//       res.render("list", { error: true });
+//     });
+// });
 
-app.post("/", async (req, res) => {
-  n = req.body.namee;
-  desc = req.body.description;
+// app.post("/", async (req, res) => {
+//   n = req.body.namee;
+//   desc = req.body.description;
 
-  const item = new Task({
-    name: n,
-    description: desc,
-    date: new Date().toLocaleString().split(",")[0],
-    time: new Date().getTime(),
-  });
-  await item.save();
-  res.redirect("/");
-});
+//   const item = new Task({
+//     name: n,
+//     description: desc,
+//     date: new Date().toLocaleString().split(",")[0],
+//     time:
+//       new Date().getHours() +
+//       " : " +
+//       new Date().getMinutes() +
+//       " : " +
+//       new Date().getSeconds(),
+//   });
+//   await item.save();
+//   res.redirect("/");
+// });
 
-app.get("/edit/:id", function (req, res, next) {
-  var id = req.params.id;
-  Task.findById(id)
-    .then((data) => {
-      res.render("edit", { newListItem: data });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.get("/edit/:id", function (req, res, next) {
+//   var id = req.params.id;
+//   Task.findById(id)
+//     .then((data) => {
+//       res.render("edit", { newListItem: data });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.post("/update", async (req, res) => {
-  var dataRecords = {
-    name: req.body.uname,
-    description: req.body.desc,
-  };
+// app.post("/update", async (req, res) => {
+//   var dataRecords = {
+//     name: req.body.uname,
+//     description: req.body.desc,
+//   };
 
-  await Task.findByIdAndUpdate(req.body.id, dataRecords)
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//   await Task.findByIdAndUpdate(req.body.id, dataRecords)
+//     .then(() => {
+//       res.redirect("/");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.get("/delete/:id", async (req, res) => {
-  var id = req.params.id;
-  await Task.findByIdAndRemove(id, (err) => {
-    if (!err) {
-      console.log("Successfully deleted");
-      res.redirect("/");
-    }
-  });
-});
+// app.get("/delete/:id", async (req, res) => {
+//   var id = req.params.id;
+//   await Task.findByIdAndRemove(id, (err) => {
+//     if (!err) {
+//       console.log("Successfully deleted");
+//       res.redirect("/");
+//     }
+//   });
+// });
 
 app.listen(3001, () => {
   console.log("listening on port 3000.");
--- src/router/task.js
+++ src/router/task.js
@@ -1,8 +1,75 @@
 const express = require("express");
+const Task = require("../db/models");
 const router = new express.Router();
 
 router.get("/test", (req, res) => {
   res.send("From a new file");
+});
+
+router.get("/", (req, res) => {
+  // Promise.reject()
+  Task.find({})
+    .then((newListItem) => {
+      res.render("list", { newListItem });
+    })
+    .catch((e) => {
+      res.render("list", { error: true });
+    });
+});
+
+router.post("/", async (req, res) => {
+  n = req.body.namee;
+  desc = req.body.description;
+
+  const item = new Task({
+    name: n,
+    description: desc,
+    date: new Date().toLocaleString().split(",")[0],
+    time:
+      new Date().getHours() +
+      " : " +
+      new Date().getMinutes() +
+      " : " +
+      new Date().getSeconds(),
+  });
+  await item.save();
+  res.redirect("/");
+});
+
+router.get("/edit/:id", function (req, res, next) {
+  var id = req.params.id;
+  Task.findById(id)
+    .then((data) => {
+      res.render("edit", { newListItem: data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.put("/update", async (req, res) => {
+  var dataRecords = {
+    name: req.body.uname,
+    description: req.body.desc,
+  };
+
+  await Task.findByIdAndUpdate(req.body.id, dataRecords)
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/delete/:id", async (req, res) => {
+  var id = req.params.id;
+  await Task.findByIdAndRemove(id, (err) => {
+    if (!err) {
+      console.log("Successfully deleted");
+      res.redirect("/");
+    }
+  });
 });
 
 module.exports = router;
